f4d826e63cbbd636d2e011f5cfdc562341cc9ca6
HafsaAouaichia <h.aouaichia@esi-sba.dz>
2024-06-06 12:30:47 +0100

    director dashboard

--- client/src/components/DashDirector/DashDirector.jsx
+++ client/src/components/DashDirector/DashDirector.jsx
@@ -1,9 +1,9 @@
 import React , {useState} from 'react'
 import Side from '../side/side';
 import Nav from '../nav/nav';
-import Total from '../DashComponents/Total';
+import TopConsumer from '../DashComponents/Total';
 import Circle from '../DashComponents/Circle';
-import Stock from '../DashComponents/Stock';
+import Barchart from '../DashComponents/histogramme';
 import Graph from '../DashComponents/MostGraphs';
 import { LuClipboardList } from "react-icons/lu";
 import { MdOutlineLocalGroceryStore } from "react-icons/md";
@@ -12,11 +12,10 @@ import { GrDeliver } from "react-icons/gr";
 import { FaCircle } from "react-icons/fa";
 
 function DashDirector() {
-    const [ReturnedDischarge, setReturnedDischarge] = useState(75);
-    const [TotalinternalOrders, setTotalinternalOrders] = useState(258);
-    const [TotalExternalOrders, setTotalExternalOrders] = useState(258);
-    const [TotalReceipts, setReceipts] = useState(40);
-    const [TotalProducts, setTotalProducts] = useState(4000);
+
+    const [total, setTotal] = useState(255);
+    const [TopConsumerName, setTopConsumer] = useState('Sidi Mohamed Benslimane');
+
 
       return (
 <div> 
@@ -24,39 +23,22 @@ function DashDirector() {
     <div className='dwnusers'>
       <Side    link='commands'/>    
       <div   style={{ marginTop :"8vh" , marginLeft :' 7%' , width :'90%', height :'92vh' , padding :'10px'}}   >
-            <div className='crcmd1' style={{display :'flex ' , alignItems: 'center', justifyContent:  'space-between'   }}>
-                <div style={{display :'flex',height :'  auto '  , borderRadius : '20px' ,width:'100%',gap : '20px' ,   boxShadow: '0px 4px 14px rgba(0, 0, 0, 0.1)' ,padding : '2px' , marginBottom :'10px' }}>
-                  <Total total={TotalinternalOrders}  item ='Total internal Orders'  icon={<LuClipboardList />} />
-                  <div style={{width: '1px',height: '50px',marginTop :'12px',  backgroundColor: '#ccc'}} />
-                  <Total total={TotalProducts}  item ='Total Products'  icon={<PiStorefront />}  />
-                  <div style={{width: '1px',height: '50px',marginTop :'12px',  backgroundColor: '#ccc'}} />
-                  <Total total={TotalExternalOrders} item ='Total External Orders'  icon={<MdOutlineLocalGroceryStore />} />
-                  <div style={{width: '1px',height: '50px',marginTop :'12px',  backgroundColor: '#ccc'}} />
-                  <Total total={TotalReceipts}  item ='Total Receipts'   icon={<GrDeliver />}  />
-                </div>
-            </div>
-
+                <div style={{display :'flex', borderRadius : '20px' ,width:'30%',boxShadow: '0px 4px 14px rgba(0, 0, 0, 0.1)' ,padding : '10px' , marginBottom :'10px' , marginLeft :'35%'}}>
+                  <TopConsumer  Title={ 'Top Consumer'} item={TopConsumerName}  total ={total}  unit  ='product' service ='service des oeuvres socials ' />
+                    </div>
+               
             <div className='crcmd1' style={{display :'flex ' , gap:'20px' }}>
 
-         <div style={{ width: '78% ', height: '260px' ,   boxShadow: '0px 4px 14px rgba(0, 0, 0, 0.1)',borderRadius:'20px', padding :'20px' }}>
-         <div style={{  color:'#333333', fontSize :'20px' , marginLeft :'40%'}}> <strong>Stock Analytics </strong></div> 
-                       <Stock />
+         <div style={{ width: '100% ', height: '450px' ,   boxShadow: '0px 4px 14px rgba(0, 0, 0, 0.1)',borderRadius:'20px', paddingTop :'20px' }}>
+          <div style={{ display :'flex'}}>  <Barchart />
+                      </div>
+                     
+
         </div>
 
-    <div style={{ width: '20%', height: '260px' ,   boxShadow: '0px 4px 14px rgba(0, 0, 0, 0.1)',borderRadius:'20px' ,  padding:'20px 50px'}}>
-    <div style={{  color:'#333333', fontSize :'20px' , marginBottom :'10px', marginLeft :'8%'}}> <strong>Dischrge status</strong></div> 
-      <Circle DischargeReturned = {ReturnedDischarge}   />  
-           <div style={{ color :'#557BDD' , fontSize :'15px' , marginTop :'5px' }}>
-           <FaCircle  style={{fontSize:'10px', marginRight :'8px'}}/>
-             Returned Discharge </div> 
-
-    </div>
-
-
-   
+  
        </div>
 
-    <Graph />
 
         </div>
 
