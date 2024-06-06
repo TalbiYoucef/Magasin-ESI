4992a0a317714f8deca0224beaead33ac24de095
HafsaAouaichia <h.aouaichia@esi-sba.dz>
2024-06-06 14:15:31 +0100

    Chef Service , service achat  dashboard ( meme dash pour les deux )

--- client/src/components/DashChafService/DashChefService.jsx
+++ client/src/components/DashChafService/DashChefService.jsx
@@ -1,22 +1,15 @@
 import React , {useState} from 'react'
 import Side from '../side/side';
 import Nav from '../nav/nav';
-import Total from '../DashComponents/Total';
-import Circle from '../DashComponents/Circle';
-import Stock from '../DashComponents/StockChafService.';
-import Graph from '../DashComponents/MostGraphsChefService';
-import { LuClipboardList } from "react-icons/lu";
-import { MdOutlineLocalGroceryStore } from "react-icons/md";
-import { PiStorefront } from "react-icons/pi";
-import { GrDeliver } from "react-icons/gr";
-import { FaCircle } from "react-icons/fa";
+import TopConsumer from '../DashComponents/Total';
+import Barchart from '../DashComponents/histogrammeService';
 
-function DashChefService() {
-    const [ReturnedDischarge, setReturnedDischarge] = useState(75);
-    const [TotalinternalOrders, setTotalinternalOrders] = useState(258);
-    const [TotalExternalOrders, setTotalExternalOrders] = useState(258);
-    const [TotalReceipts, setReceipts] = useState(40);
-    const [TotalProducts, setTotalProducts] = useState(4000);
+
+function DashDirector() {
+
+    const [total, setTotal] = useState(255);
+    const [TopServiceConsumerName, setTopServiceConsumerName] = useState('Sidi Mohamed Benslimane');
+
 
       return (
 <div> 
@@ -24,24 +17,22 @@ function DashChefService() {
     <div className='dwnusers'>
       <Side    link='commands'/>    
       <div   style={{ marginTop :"8vh" , marginLeft :' 7%' , width :'90%', height :'92vh' , padding :'10px'}}   >
-            <div className='crcmd1' style={{display :'flex ' , alignItems: 'center', justifyContent:  'space-between'   }}>
-                <div style={{display :'flex',height :'  auto '  , borderRadius : '20px' , marginLeft :'27%',width:'45%',gap : '20px' ,   boxShadow: '0px 4px 14px rgba(0, 0, 0, 0.1)' ,padding : '2px' , marginBottom :'10px' }}>
-                  <Total total={TotalExternalOrders} item ='Total External Orders'  icon={<MdOutlineLocalGroceryStore />} />
-                  <div style={{width: '1px',height: '50px',marginTop :'12px',  backgroundColor: '#ccc'}} />
-                  <Total total={TotalReceipts}  item ='Total Receipts'   icon={<GrDeliver />}  />
-                </div>
-            </div>
-
+                <div style={{display :'flex', borderRadius : '20px' ,width:'30%',boxShadow: '0px 4px 14px rgba(0, 0, 0, 0.1)' ,padding : '10px' , marginBottom :'10px' , marginLeft :'35%'}}>
+                  <TopConsumer  Title={ 'Top Consumer'} item={TopServiceConsumerName}  total ={total}  unit  ='product'   />
+                    </div>
+               
             <div className='crcmd1' style={{display :'flex ' , gap:'20px' }}>
 
-         <div style={{ width: '70% ', height: '260px' , marginLeft :'15%' ,  boxShadow: '0px 4px 14px rgba(0, 0, 0, 0.1)',borderRadius:'20px', padding :'20px' }}>
-         <div style={{  color:'#333333', fontSize :'20px' , marginLeft :'40%'}}> <strong>Stock Analytics </strong></div> 
-                       <Stock />
+         <div style={{ width: '100% ', height: '450px' ,   boxShadow: '0px 4px 14px rgba(0, 0, 0, 0.1)',borderRadius:'20px', paddingTop :'20px' }}>
+          <div style={{ display :'flex'}}>  <Barchart />
+                      </div>
+                     
+
         </div>
 
+  
        </div>
 
-    <Graph />
 
         </div>
 
@@ -51,4 +42,4 @@ function DashChefService() {
   )
 }
 
-export default DashChefService
+export default DashDirector
---
+++ client/src/components/DashComponents/histogrammeService.jsx
@@ -0,0 +1,345 @@
+import React, { useState, useEffect } from "react";
+import Chart from "react-apexcharts";
+
+function Barchart() {
+  const [CurentService, setCurentService] = useState("Service A");
+
+  const [MostServiceConsumer, setMostServiceConsumer] = useState([
+    { Service: "Service A", consumer: "Consumer 1A" },
+  
+  ]);
+
+  const [MostConsumerProduct, setMostConsumerProduct] = useState([
+    { consumer: "Consumer 1A", Product: "papier" },
+    { consumer: "Consumer 2A", Product: "Product 4" },
+  ]);
+
+  const [ServicesData, setServicesData] = useState([
+    {
+      name: "Service A",
+      data: [6578, 6787, 3245, 9876, 2324, 5123, 2435, 1987, 1376, 3245, 9876, 2324],
+      consumers: [
+        {
+          name: "Consumer 1A",
+          data: [1234, 5678, 987, 5432, 8765, 2345, 6789, 3456, 7890, 1234, 5678, 987],
+          products: [
+            { name: "papier", data: [4567, 8901, 2345, 6789, 1234, 5678, 9876, 5432, 8765, 2345, 6789, 1234] },
+            { name: "Product 2", data: [7890, 1234, 5678, 9876, 5432, 8765, 2345, 6789, 1234, 5678, 9876, 5432] },
+          ],
+        },
+        {
+          name: "Consumer 2A",
+          data: [/* 12 values */],
+          products: [
+            { name: "Product 3", data: [/* 12 values */] },
+            { name: "Product 4", data: [7890, 1234, 5678, 9876, 5432, 8765, 2345, 6789, 1234, 5678, 9876, 5432]  },
+          ],
+        },
+      ],
+    },    
+  ]);
+
+  const [selectedRangeConsumer, setSelectedRangeConsumer] = useState("MostConsumer");
+  const [selectedRangeProduct, setSelectedRangeProduct] = useState("MostProduct");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const selectedServiceObject = ServicesData.find((service) => service.name === CurentService);
+    const mostConsumerInService = MostServiceConsumer.find((service) => service.Service === selectedServiceObject?.name)?.consumer || "";
+    const selectedConsumerObject = selectedRangeConsumer === "MostConsumer" ? selectedServiceObject?.consumers.find((consumer) => consumer.name === mostConsumerInService) : selectedServiceObject?.consumers.find((consumer) => consumer.name === selectedRangeConsumer);
+
+    setFilteredProducts(selectedConsumerObject?.products || []);
+
+    // Update selectedRangeConsumer and selectedRangeProduct when the selectedRange changes
+    
+    if (selectedRangeConsumer === "MostConsumer") {
+      setSelectedRangeProduct("MostProduct");
+    }
+  }, [selectedRangeConsumer, ServicesData, MostServiceConsumer]);
+
+  const selectedServiceObject =  ServicesData.find((service) => service.name === CurentService);
+  const selectedData = selectedServiceObject?.data || [];
+  const selectedService = selectedServiceObject?.name || "";
+
+  const mostConsumerInService = MostServiceConsumer.find((service) => service.Service === selectedService)?.consumer || "";
+  const selectedConsumerObject = selectedRangeConsumer === "MostConsumer" ? selectedServiceObject?.consumers.find((consumer) => consumer.name === mostConsumerInService) : selectedServiceObject?.consumers.find((consumer) => consumer.name === selectedRangeConsumer);
+  const selectedConsumerData = selectedConsumerObject?.data || [];
+  const selectedConsumerName = selectedConsumerObject?.name || "";
+
+  const mostProductInConsumer = MostConsumerProduct.find((product) => product.consumer === selectedConsumerObject?.name)?.Product || "";
+  const selectedProductObject = selectedRangeProduct === "MostProduct" ? selectedConsumerObject?.products.find((product) => product.name === mostProductInConsumer) : selectedConsumerObject?.products.find((product) => product.name === selectedRangeProduct);
+  const selectedProductsData = selectedProductObject?.data || [];
+  const selectedProductsName = selectedProductObject?.name || "";
+
+
+  const handleChangeConsumer = (e) => {
+    setSelectedRangeConsumer(e.target.value);
+  };
+  const handleChangeProduct = (e) => {
+    setSelectedRangeProduct(e.target.value);
+  };
+
+  const handleSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    const filteredProducts = selectedConsumerObject?.products.filter((product) =>
+      product.name.toLowerCase().includes(searchTerm)
+    );
+    setFilteredProducts(filteredProducts || []);
+  };
+  return (
+    <React.Fragment>
+      <div className="container-fluid mb-5">
+       
+        
+        <Chart
+          type="bar"
+          width={400}
+          height={400}
+          series={[
+            {
+              name: "Total Products",
+              data: selectedData,
+            },
+          ]}
+          options={{
+            title: {
+              text: "Service",
+              style: { fontSize: 20 },
+            },
+            subtitle: {
+              text: selectedService,
+              style: { fontSize: 16 },
+            },
+            colors: ["#FFA500"],
+            theme: { mode: "light" },
+            xaxis: {
+              tickPlacement: "on",
+              categories: [
+                "janvier",
+                "février",
+                "mars",
+                "avril",
+                "mai",
+                "juin",
+                "juillet",
+                "août",
+                "septembre",
+                "octobre",
+                "novembre",
+                "décembre",
+              ],
+            },
+            yaxis: {
+              labels: {
+                formatter: (val) => {
+                  return `${val}`;
+                },
+                style: { fontSize: "15", colors: ["#616262"] },
+              },
+              title: {
+                text: "Total Products",
+                style: { color: "#616262", fontSize: 16 },
+              },
+            },
+            legend: {
+              show: true,
+              position: "right",
+            },
+            dataLabels: {
+              enabled: false,
+            },
+          }}
+        />
+      </div>
+
+      <div className="container-fluid mb-5">
+        <select
+          value={selectedRangeConsumer}
+          onChange={handleChangeConsumer}
+          style={{
+            padding: "2px",
+            borderRadius: "20px",
+            border: "1px solid #ced4da",
+            boxShadow: "0px 1px 4px rgba(0, 0, 0, 0.25)",
+            fontSize: "12px",
+            outline: "none",
+            textAlign: "left",
+            width: "150px",
+            marginLeft: '250px',
+            color: 'black'
+          }}
+        >
+          <option value="MostConsumer">MostConsumer</option>
+          {selectedServiceObject?.consumers.map((consumer) => (
+            <option key={consumer.name} value={consumer.name}>
+              {consumer.name}
+            </option>
+          ))}
+        </select>
+        <Chart
+          type="bar"
+          width={400}
+          height={400}
+          series={[
+            {
+              name: "Total Products",
+              data: selectedConsumerData,
+            },
+          ]}
+          options={{
+            title: {
+              text: "Consumers",
+              style: { fontSize: 20 },
+            },
+            subtitle: {
+              text: selectedConsumerName,
+              style: { fontSize: 16 },
+            },
+            colors: ["#00b964"],
+            theme: { mode: "light" },
+            xaxis: {
+              tickPlacement: "on",
+              categories: [
+                "janvier",
+                "février",
+                "mars",
+                "avril",
+                "mai",
+                "juin",
+                "juillet",
+                "août",
+                "septembre",
+                "octobre",
+                "novembre",
+                "décembre",
+              ],
+            },
+            yaxis: {
+              labels: {
+                formatter: (val) => {
+                  return `${val}`;
+                },
+                style: { fontSize: "15", colors: ["#616262"] },
+              },
+              title: {
+                text: "Total Products",
+                style: { color: "#616262", fontSize: 16 },
+              },
+            },
+            legend: {
+              show: true,
+              position: "right",
+            },
+            dataLabels: {
+              enabled: false,
+            },
+          }}
+        />
+      </div>
+
+      <div className="container-fluid mb-5">
+        <div style={{ display: 'flex' }}>
+          <input
+            type="text"
+            placeholder="Rechercher un produit"
+            onChange={handleSearch}
+            style={{
+              padding: "2px",
+              paddingLeft: '4px',
+              borderRadius: "10px",
+              border: "1px solid #ced4da",
+              boxShadow: "0px 1px 4px rgba(0, 0, 0, 0.25)",
+              fontSize: "12px",
+              outline: "none",
+              textAlign: "left",
+              width: "150px",
+            }}
+          />
+          <select
+            value={selectedRangeProduct}
+            onChange={handleChangeProduct}
+            style={{
+              padding: "2px",
+              borderRadius: "20px",
+              border: "1px solid #ced4da",
+              boxShadow: "0px 1px 4px rgba(0, 0, 0, 0.25)",
+              fontSize: "12px",
+              outline: "none",
+              textAlign: "left",
+              width: "150px",
+              color: 'black'
+            }}
+          >
+            <option value="MostProduct">MostProduct</option>
+            {filteredProducts?.map((product) => (
+              <option key={product.name} value={product.name}>
+                {product.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <Chart
+          type="bar"
+          width={400}
+          height={400}
+          series={[
+            {
+              name: "Total Products",
+              data: selectedProductsData,
+            },
+          ]}
+          options={{
+            title: {
+              text: "Products",
+              style: { fontSize: 20 },
+            },
+            subtitle: {
+              text: selectedProductsName,
+              style: { fontSize: 16 },
+            },
+            colors: ["#01b3d8"],
+            theme: { mode: "light" },
+            xaxis: {
+              tickPlacement: "on",
+              categories: [
+                "janvier",
+                "février",
+                "mars",
+                "avril",
+                "mai",
+                "juin",
+                "juillet",
+                "août",
+                "septembre",
+                "octobre",
+                "novembre",
+                "décembre",
+              ],
+            },
+            yaxis: {
+              labels: {
+                formatter: (val) => {
+                  return `${val}`;
+                },
+                style: { fontSize: "15", colors: ["#616262"] },
+              },
+              title: {
+                text: "Total Products",
+                style: { color: "#616262", fontSize: 16 },
+              },
+            },
+            legend: {
+              show: true,
+              position: "right",
+            },
+            dataLabels: {
+              enabled: false,
+            },
+          }}
+        />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Barchart;
